954e3f77940ffb45cdbeb4cd350616b70ef5a08f
Raghavendra Kuruva <229x1a05k4@gprec.ac.in>
2026-07-31 00:15:13 +0530

    Time: 0 ms (100%), Space: 43.2 MB (66.51%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,11 +1,15 @@
 class Solution {
     public int minimumPushes(String word) {
-        int ans = 0;
+        int n = word.length();
 
-        for (int i = 0; i < word.length(); i++) {
-            ans += (i / 8) + 1;
-        }
+        int first = Math.min(n, 8);
+        int second = Math.min(Math.max(n - 8, 0), 8);
+        int third = Math.min(Math.max(n - 16, 0), 8);
+        int fourth = Math.max(n - 24, 0);
 
-        return ans;
+        return first
+             + second * 2
+             + third * 3
+             + fourth * 4;
     }
 }
